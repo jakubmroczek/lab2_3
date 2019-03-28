c8a4aeb592a35903868996583e07781183031e98
Zandros229 <ernest.figura548@gmail.com>
2019-03-28 17:50:18 +0100

    2.3 SimilarityFinder add SimilarityFinderTestSeqOfZeroSize test

--- src/test/java/SimilarityFinderTest.java
+++ src/test/java/SimilarityFinderTest.java
@@ -3,11 +3,20 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class SimilarityFinderTest {
+
     @Test
     public void SimilarityFinderTestGoodValue(){
         SimilarityFinder finder=new SimilarityFinder();
         int tab1[]={1,2,3,4,5,6};
         int tab2[]={5,4,6,7,8,9};
         Assertions.assertEquals(0.5,finder.calculateJackardSimilarity(tab1,tab2));
+    }
+
+    @Test
+    public void SimilarityFinderTestSeqOfZeroSize(){
+        SimilarityFinder finder=new SimilarityFinder();
+        int tab1[]={};
+        int tab2[]={};
+        Assertions.assertEquals(1.0,finder.calculateJackardSimilarity(tab1,tab2));
     }
 }
