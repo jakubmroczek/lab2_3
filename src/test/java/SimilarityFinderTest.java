11f54ff9d5eb5c07b4ea8a9744be2a85eb2ea2b4
Zandros229 <ernest.figura548@gmail.com>
2019-03-28 17:48:24 +0100

    2.3 SimilarityFinder add SimilarityFinderTestGoodValue

---
+++ src/test/java/SimilarityFinderTest.java
@@ -0,0 +1,13 @@
+import edu.iis.mto.similarity.SimilarityFinder;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SimilarityFinderTest {
+    @Test
+    public void SimilarityFinderTestGoodValue(){
+        SimilarityFinder finder=new SimilarityFinder();
+        int tab1[]={1,2,3,4,5,6};
+        int tab2[]={5,4,6,7,8,9};
+        Assertions.assertEquals(0.5,finder.calculateJackardSimilarity(tab1,tab2));
+    }
+}
