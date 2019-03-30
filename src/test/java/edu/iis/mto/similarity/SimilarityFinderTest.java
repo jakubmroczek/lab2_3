77cceff9cc47f2bbec7a7604b1f8b50ed75a27e8
Fabicki.Maciej <209305@edu.p.lodz.pl>
2019-03-30 18:02:35 +0100

    test for SimiarityFinder for completely different sequences

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -19,4 +19,16 @@ public class SimilarityFinderTest {
         assertThat(result, Matchers.equalTo(1.0));
 
     }
+
+
+    @Test
+    public void shouldReturnZeroIfSetsAreCompletelyDifferent() {
+
+        int[] seq1 = {1, 2, 3, 4, 5};
+        int[] seq2 = {6, 7, 8, 9};
+
+        SimilarityFinder similarityFinder = new SimilarityFinder(new SequenceSearcherDoubler());
+        double result = similarityFinder.calculateJackardSimilarity(seq1, seq2);
+        assertThat(result, Matchers.equalTo(0.0));
+    }
 }
