3cf871a0cbea6d346c0b87e559285c58e2bd78d1
Michał Grzelak <grzelakm@outlook.com>
2019-04-01 23:01:37 +0200

    Added shouldReturnZeroIfSequencesAreDifferent test

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -31,4 +31,12 @@ public class SimilarityFinderTest {
 
         assertEquals(1.0d, this.similarityFinder.calculateJackardSimilarity(seq1, seq2), 0);
     }
+
+    @Test
+    public void shouldReturnZeroIfSequencesAreDifferent() {
+        int[] seq1 = {3, 1, 7};
+        int[] seq2 = {4, 2, 8};
+
+        assertEquals(0.0d, this.similarityFinder.calculateJackardSimilarity(seq1, seq2), 0);
+    }
 }
