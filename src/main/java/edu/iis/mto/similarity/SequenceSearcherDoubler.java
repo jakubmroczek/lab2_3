de3a8a6bab73c50949512c8eab3f5b3c8f85cf6b
Michał Grzelak <grzelakm@outlook.com>
2019-04-01 22:41:28 +0200

    Implemented SequenceSearcherDoubler

--- src/main/java/edu/iis/mto/similarity/SequenceSearcherDoubler.java
+++ src/main/java/edu/iis/mto/similarity/SequenceSearcherDoubler.java
@@ -1,5 +1,23 @@
 package edu.iis.mto.similarity;
 
-public class SequenceSearcherDoubler {
+import edu.iis.mto.search.SearchResult;
+import edu.iis.mto.search.SequenceSearcher;
 
+public class SequenceSearcherDoubler implements SequenceSearcher {
+
+    @Override
+    public SearchResult search(int key, int[] seq) {
+        SearchResult.Builder builder = SearchResult.builder();
+
+        if(seq != null) {
+            for(int i = 0; i < seq.length; i++) {
+                if(key == seq[i]) {
+                    builder.withFound(true);
+                    builder.withPosition(i);
+                }
+            }
+        }
+
+        return builder.build();
+    }
 }
