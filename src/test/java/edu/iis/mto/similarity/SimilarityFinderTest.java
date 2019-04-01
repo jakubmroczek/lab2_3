244bb10637482f1f7d99a38e0823b66af8e92d5f
Michał Grzelak <grzelakm@outlook.com>
2019-04-01 22:44:42 +0200

    Added init method in tests

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -1,7 +1,17 @@
 package edu.iis.mto.similarity;
 
+import edu.iis.mto.search.SequenceSearcher;
+import org.junit.Before;
+
 import static org.junit.Assert.*;
 
 public class SimilarityFinderTest {
+    private SequenceSearcher sequenceSearcher;
+    private SimilarityFinder similarityFinder;
 
+    @Before
+    public void init() {
+        this.sequenceSearcher = new SequenceSearcherDoubler();
+        this.similarityFinder = new SimilarityFinder(sequenceSearcher);
+    }
 }
