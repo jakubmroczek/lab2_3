f849c7d96b4c82a1cbadfe737e65bffd084899c4
adamsylla97 <209474@edu.p.lodz.pl>
2019-04-07 14:49:16 +0200

    *added sequenceSearcherDoubler *fixed similariyTests with usage of doubler class

--- src/test/java/SimilarityFinderTest.java
+++ src/test/java/SimilarityFinderTest.java
@@ -1,13 +1,25 @@
+import edu.iis.mto.search.SearchResult;
+import edu.iis.mto.search.SequenceSearcher;
+import edu.iis.mto.similarity.SequenceSearcherDoubler;
 import edu.iis.mto.similarity.SimilarityFinder;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class SimilarityFinderTest {
 
     @Test
-    public static void calculateJackardSimilarityTest(){
+    public void calculateJackardSimilarityWithSameSequencesTest(){
 
-        SequenceSercher sequenceSercher = new SequenceSercher();
+        Map<Integer, SearchResult> valueMap = new HashMap<>();
+        valueMap.put(1, SearchResult.builder().withFound(true).build());
+        valueMap.put(2, SearchResult.builder().withFound(true).build());
+        valueMap.put(3, SearchResult.builder().withFound(true).build());
+        valueMap.put(4, SearchResult.builder().withFound(true).build());
+
+        SequenceSearcherDoubler sequenceSercher = new SequenceSearcherDoubler(valueMap);
 
         SimilarityFinder similarityFinder = new SimilarityFinder(sequenceSercher);
 
@@ -15,21 +27,14 @@ public class SimilarityFinderTest {
         int[] seq2 = {1,2,3,4};
 
         Assertions.assertEquals(1,similarityFinder.calculateJackardSimilarity(seq1,seq2));
-
-        int[] seq3 = {1,2,5,6};
-        int[] seq4 = {1,2,7,8};
-
-        Assertions.assertEquals(0.5,similarityFinder.calculateJackardSimilarity(seq3,seq4));
-
-
     }
 
     @Test
-    public static void calculateJackardSimilarityWithSequencesLenZeroTest(){
+    public void calculateJackardSimilarityWithSequencesLenZeroTest(){
 
-        SequenceSercher sequenceSercher = new SequenceSercher();
+        SequenceSearcherDoubler sequenceSearcherDoubler = new SequenceSearcherDoubler();
 
-        SimilarityFinder similarityFinder = new SimilarityFinder(sequenceSercher);
+        SimilarityFinder similarityFinder = new SimilarityFinder(sequenceSearcherDoubler);
 
         int[] seq1 = {};
         int[] seq2 = {};
