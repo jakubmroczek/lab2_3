47f5369f256d6931c70c0eda75f9f7be4fcf3729
adamsylla97 <209474@edu.p.lodz.pl>
2019-04-07 21:09:01 +0200

    *code formatting

--- src/test/java/SimilarityFinderTest.java
+++ src/test/java/SimilarityFinderTest.java
@@ -10,7 +10,7 @@ import java.util.Map;
 class SimilarityFinderTest {
 
     @Test
-    void calculateJackardSimilarityWithSameSequencesTest(){
+    void calculateJackardSimilarityWithSameSequencesTest() {
 
         Map<Integer, SearchResult> valueMap = new HashMap<>();
         valueMap.put(1, SearchResult.builder().withFound(true).build());
@@ -22,14 +22,14 @@ class SimilarityFinderTest {
 
         SimilarityFinder similarityFinder = new SimilarityFinder(sequenceSercher);
 
-        int[] seq1 = {1,2,3,4};
-        int[] seq2 = {1,2,3,4};
+        int[] seq1 = {1, 2, 3, 4};
+        int[] seq2 = {1, 2, 3, 4};
 
-        Assertions.assertEquals(1,similarityFinder.calculateJackardSimilarity(seq1,seq2));
+        Assertions.assertEquals(1, similarityFinder.calculateJackardSimilarity(seq1, seq2));
     }
 
     @Test
-    void calculateJackardSimilarityWithSequencesLenZeroTest(){
+    void calculateJackardSimilarityWithSequencesLenZeroTest() {
 
         SequenceSearcherDoubler sequenceSearcherDoubler = new SequenceSearcherDoubler();
 
@@ -38,12 +38,12 @@ class SimilarityFinderTest {
         int[] seq1 = {};
         int[] seq2 = {};
 
-        Assertions.assertEquals(1,similarityFinder.calculateJackardSimilarity(seq1,seq2));
+        Assertions.assertEquals(1, similarityFinder.calculateJackardSimilarity(seq1, seq2));
 
     }
 
     @Test
-    void calculateJackardSimilarityWithDifferentSequences(){
+    void calculateJackardSimilarityWithDifferentSequences() {
 
         Map<Integer, SearchResult> valueMap = new HashMap<>();
         valueMap.put(0, SearchResult.builder().withFound(true).build());
@@ -58,16 +58,15 @@ class SimilarityFinderTest {
 
         SimilarityFinder similarityFinder = new SimilarityFinder(sequenceSearcherDoubler);
 
-        int[] seq1 = {0,1,2,5,6,8,10};
-        int[] seq2 = {0,2,3,4,5,10,11};
+        int[] seq1 = {0, 1, 2, 5, 6, 8, 10};
+        int[] seq2 = {0, 2, 3, 4, 5, 10, 11};
 
-        Assertions.assertEquals(0.4,similarityFinder.calculateJackardSimilarity(seq1,seq2));
+        Assertions.assertEquals(0.4, similarityFinder.calculateJackardSimilarity(seq1, seq2));
 
     }
 
-
     @Test
-    void calculateJackardSimilarityWithDifferentSizesAndSequences(){
+    void calculateJackardSimilarityWithDifferentSizesAndSequences() {
 
         Map<Integer, SearchResult> valueMap = new HashMap<>();
         valueMap.put(0, SearchResult.builder().withFound(true).build());
@@ -80,15 +79,15 @@ class SimilarityFinderTest {
 
         SimilarityFinder similarityFinder = new SimilarityFinder(sequenceSearcherDoubler);
 
-        int[] seq1 = {0,1,2,5,6};
-        int[] seq2 = {0,2,3,4,5,7};
+        int[] seq1 = {0, 1, 2, 5, 6};
+        int[] seq2 = {0, 2, 3, 4, 5, 7};
 
-        Assertions.assertEquals(0.375,similarityFinder.calculateJackardSimilarity(seq1,seq2));
+        Assertions.assertEquals(0.375, similarityFinder.calculateJackardSimilarity(seq1, seq2));
 
     }
 
     @Test
-    void calculateIntersectForEmptySequencesTest(){
+    void calculateIntersectForEmptySequencesTest() {
 
         SequenceSearcherDoubler sequenceSearcherDoubler = new SequenceSearcherDoubler();
 
@@ -97,14 +96,14 @@ class SimilarityFinderTest {
         int[] seq1 = {};
         int[] seq2 = {};
 
-        similarityFinder.calculateJackardSimilarity(seq1,seq2);
+        similarityFinder.calculateJackardSimilarity(seq1, seq2);
 
-        Assertions.assertEquals(0,sequenceSearcherDoubler.getCounter());
+        Assertions.assertEquals(0, sequenceSearcherDoubler.getCounter());
 
     }
 
     @Test
-    void calculateIntersectForSequencesWithNumbersInsideTest(){
+    void calculateIntersectForSequencesWithNumbersInsideTest() {
 
         Map<Integer, SearchResult> valueMap = new HashMap<>();
         valueMap.put(0, SearchResult.builder().withFound(true).build());
@@ -117,15 +116,13 @@ class SimilarityFinderTest {
 
         SimilarityFinder similarityFinder = new SimilarityFinder(sequenceSearcherDoubler);
 
-        int[] seq1 = {0,1,2,5,6};
-        int[] seq2 = {0,2,3,4,5,7};
+        int[] seq1 = {0, 1, 2, 5, 6};
+        int[] seq2 = {0, 2, 3, 4, 5, 7};
 
-        similarityFinder.calculateJackardSimilarity(seq1,seq2);
+        similarityFinder.calculateJackardSimilarity(seq1, seq2);
 
-        Assertions.assertEquals(3,sequenceSearcherDoubler.getCounter());
+        Assertions.assertEquals(3, sequenceSearcherDoubler.getCounter());
 
     }
-
-
 
 }
