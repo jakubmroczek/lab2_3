4033b1f05c2bed8e78d947cd240ebc3ebf6506ac
Zandros229 <ernest.figura548@gmail.com>
2019-04-11 11:18:37 +0200

    Add SmilarityFinderTest class that simulate SequenceSearcher

--- src/test/java/SimilarityFinderTest.java
+++ src/test/java/SimilarityFinderTest.java
@@ -1,35 +1,133 @@
+import edu.iis.mto.search.SearchResult;
+import edu.iis.mto.similarity.SequenceSearcherForTest;
 import edu.iis.mto.similarity.SimilarityFinder;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class SimilarityFinderTest {
 
     @Test
-    public void SimilarityFinderTestGoodValue(){
-        SimilarityFinder finder=new SimilarityFinder();
-        int tab1[]={1,2,3,4,5,6};
-        int tab2[]={5,4,6,7,8,9};
-        Assertions.assertEquals(0.5,finder.calculateJackardSimilarity(tab1,tab2));
+    public void SameSequencesJackardSimilarityTest() {
+
+        Map<Integer, SearchResult> map = new HashMap<>();
+        map.put(1, SearchResult.builder().withFound(true).build());
+        map.put(2, SearchResult.builder().withFound(true).build());
+        map.put(3, SearchResult.builder().withFound(true).build());
+        map.put(4, SearchResult.builder().withFound(true).build());
+        map.put(5, SearchResult.builder().withFound(true).build());
+        map.put(6, SearchResult.builder().withFound(true).build());
+        map.put(7, SearchResult.builder().withFound(true).build());
+
+        SequenceSearcherForTest sequenceSercher = new SequenceSearcherForTest(map);
+
+        SimilarityFinder similarityFinder = new SimilarityFinder(sequenceSercher);
+
+        int[] seq1 = {1, 2, 3, 4, 5,6,7};
+        int[] seq2 = {1, 2, 3, 4, 5,6,7};
+
+        Assertions.assertEquals(1, similarityFinder.calculateJackardSimilarity(seq1, seq2));
     }
 
     @Test
-    public void SimilarityFinderTestSeqOfZeroSize(){
-        SimilarityFinder finder=new SimilarityFinder();
-        int tab1[]={};
-        int tab2[]={};
-        Assertions.assertEquals(1.0,finder.calculateJackardSimilarity(tab1,tab2));
+    public void sequencesZeroLenJackardSimilarityTest() {
+
+        SequenceSearcherForTest sequenceSearcherForTest = new SequenceSearcherForTest();
+
+        SimilarityFinder similarityFinder = new SimilarityFinder(sequenceSearcherForTest);
+
+        int[] seq1 = {};
+        int[] seq2 = {};
+
+        Assertions.assertEquals(1, similarityFinder.calculateJackardSimilarity(seq1, seq2));
+
     }
 
     @Test
-    public void SimilarityFinderTestMethodSearchInvoke(){
-        SimilarityFinder finder=new SimilarityFinder();
-        int tab1[]={1,2,3,4,5,6};
-        int tab2[]={5,4,6,7,8,9};
-        for(int i=0;i<tab1.length;i++){
+    public void differentSequencesJackardSimilarityTest() {
 
-        }
-        Assertions.assertEquals(0.5,finder.calculateJackardSimilarity(tab1,tab2));
+        Map<Integer, SearchResult> map = new HashMap<>();
+        map.put(0, SearchResult.builder().withFound(true).build());
+        map.put(1, SearchResult.builder().withFound(false).build());
+        map.put(2, SearchResult.builder().withFound(true).build());
+        map.put(3, SearchResult.builder().withFound(false).build());
+        map.put(4, SearchResult.builder().withFound(true).build());
+        map.put(5, SearchResult.builder().withFound(false).build());
+        map.put(6, SearchResult.builder().withFound(true).build());
+
+
+        SequenceSearcherForTest sequenceSearcherForTest = new SequenceSearcherForTest(map);
+
+        SimilarityFinder similarityFinder = new SimilarityFinder(sequenceSearcherForTest);
+
+        int[] seq1 = {0, 1, 2, 3, 4, 5, 6};
+        int[] seq2 = {0, 2, 4, 6, 8, 10, 12};
+
+        Assertions.assertEquals(0.4, similarityFinder.calculateJackardSimilarity(seq1, seq2));
+
     }
 
+    @Test
+    public void differentSizesAndSequencesJackardSimilarityTest() {
+
+        Map<Integer, SearchResult> map = new HashMap<>();
+        map.put(7, SearchResult.builder().withFound(true).build());
+        map.put(8, SearchResult.builder().withFound(false).build());
+        map.put(9, SearchResult.builder().withFound(true).build());
+        map.put(11, SearchResult.builder().withFound(true).build());
+        map.put(13, SearchResult.builder().withFound(false).build());
+
+        SequenceSearcherForTest sequenceSearcherForTest = new SequenceSearcherForTest(map);
+
+        SimilarityFinder similarityFinder = new SimilarityFinder(sequenceSearcherForTest);
+
+        int[] seq1 = {7, 8, 9, 11, 13};
+        int[] seq2 = {7, 9, 11, 12, 13, 15};
+
+        Assertions.assertEquals(0.375, similarityFinder.calculateJackardSimilarity(seq1, seq2));
+
+    }
+
+    @Test
+    public void EmptySequencesIntersectTest() {
+
+        SequenceSearcherForTest sequenceSearcherForTest = new SequenceSearcherForTest();
+
+        SimilarityFinder similarityFinder = new SimilarityFinder(sequenceSearcherForTest);
+
+        int[] seq1 = {};
+        int[] seq2 = {};
+
+        similarityFinder.calculateJackardSimilarity(seq1, seq2);
+
+        Assertions.assertEquals(0, sequenceSearcherForTest.getCounter());
+
+    }
+
+    @Test
+    public void sequencesWithNumbersInsideIntersectTest() {
+
+        Map<Integer, SearchResult> map = new HashMap<>();
+        map.put(0, SearchResult.builder().withFound(true).build());
+        map.put(1, SearchResult.builder().withFound(true).build());
+        map.put(3, SearchResult.builder().withFound(true).build());
+        map.put(6, SearchResult.builder().withFound(false).build());
+        map.put(7, SearchResult.builder().withFound(true).build());
+
+        SequenceSearcherForTest sequenceSearcherForTest = new SequenceSearcherForTest(map);
+
+        SimilarityFinder similarityFinder = new SimilarityFinder(sequenceSearcherForTest);
+
+        int[] seq1 = {0, 1, 3, 6, 7};
+        int[] seq2 = {0, 1, 3, 4, 5, 7};
+
+        similarityFinder.calculateJackardSimilarity(seq1, seq2);
+
+        Assertions.assertEquals(4, sequenceSearcherForTest.getCounter());
+
+    }
 
 }
