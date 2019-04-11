be930b67a54e4ea4bc8a75cdcc5efe457713d4e4
Igor Jarek <201953@edu.p.lodz.pl>
2019-04-11 03:09:24 +0200

    Lab 2_3 - Add shouldReturnZero Test

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -22,4 +22,19 @@ public class SimilarityFinderTest {
         double expected = 1.0;
         assertThat(calculatedValue, is(expected));
     }
+
+    @Test
+    public void shouldReturnZero() {
+
+        int[] seq1 = {1, 2, 3, 4};
+        int[] seq2 = {5, 6, 7, 8};
+        boolean[] seqReturn = {false, false, false, false};
+
+        SequenceSearcher sequenceSearcher = new SequenceSearcherImpl(seqReturn);
+        SimilarityFinder similarityFinder = new SimilarityFinder(sequenceSearcher);
+
+        double calculatedValue = similarityFinder.calculateJackardSimilarity(seq1, seq2);
+        double expected = 0.0;
+        assertThat(calculatedValue, is(expected));
+    }
 }
