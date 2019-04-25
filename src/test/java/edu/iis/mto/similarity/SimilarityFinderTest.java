2ef4dff417b164d3f14037e2d418108c4035cd21
Adam Dąbrowski <190139@edu.p.lodz.pl>
2019-04-25 16:31:20 +0200

    Implement test that checks for correct calculation

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -34,6 +34,18 @@ public class SimilarityFinderTest {
         assertThat(similarityFinder.calculateJackardSimilarity(seq1, seq2), is(1.0));
     }
 
+    @Test
+    public void shouldReturnCorrectIndex() {
+        Iterable<Boolean> mockedResults = Arrays.asList(true, true, false);
+        SequenceSearcherDubler sequenceSearcherDubler = new SequenceSearcherDubler(mockedResults);
+        SimilarityFinder similarityFinder = new SimilarityFinder(sequenceSearcherDubler);
+
+        int[] seq1 = {1, 3, 6};
+        int[] seq2 = {1, 3, 5};
+
+        assertThat(similarityFinder.calculateJackardSimilarity(seq1, seq2), is(0.5));
+    }
+
     @Test(expected = NullPointerException.class)
     public void shouldThrowExceptionOnNullArrays() {
         Iterable<Boolean> mockedResults = Collections.emptyList();
