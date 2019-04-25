3ee25b8fece17f91f52c0392587d11944408af83
Adam Dąbrowski <190139@edu.p.lodz.pl>
2019-04-25 16:27:55 +0200

    Implement test for equal arrays

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -2,6 +2,7 @@ package edu.iis.mto.similarity;
 
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.Collections;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -17,6 +18,18 @@ public class SimilarityFinderTest {
 
         int[] seq1 = {};
         int[] seq2 = {};
+
+        assertThat(similarityFinder.calculateJackardSimilarity(seq1, seq2), is(1.0));
+    }
+
+    @Test
+    public void shouldReturn1ForSameArrays() {
+        Iterable<Boolean> mockedResults = Arrays.asList(true, true, true);
+        SequenceSearcherDubler sequenceSearcherDubler = new SequenceSearcherDubler(mockedResults);
+        SimilarityFinder similarityFinder = new SimilarityFinder(sequenceSearcherDubler);
+
+        int[] seq1 = {1, 3, 5};
+        int[] seq2 = {1, 3, 5};
 
         assertThat(similarityFinder.calculateJackardSimilarity(seq1, seq2), is(1.0));
     }
