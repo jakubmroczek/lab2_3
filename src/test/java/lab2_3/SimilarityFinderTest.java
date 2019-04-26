19eb39a103d1ee27d9e1ff49bc5f5e8eed414e7c
rogo6 <piotrrogozinski95@gmail.com>
2019-04-26 15:16:09 +0200

    Added first test

--- src/test/java/lab2_3/SimilarityFinderTest.java
+++ src/test/java/lab2_3/SimilarityFinderTest.java
@@ -1,5 +1,34 @@
 package lab2_3;
 
-public class SimilarityFinderTest {
+import org.junit.Before;
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import edu.iis.mto.similarity.SequenceSearcherStub;
+import edu.iis.mto.similarity.SimilarityFinder;
 
+public class SimilarityFinderTest {
+	
+	SequenceSearcherStub stub;
+	SimilarityFinder finder;
+	
+	@Before
+	public void initialize() {
+		stub = new SequenceSearcherStub();
+		finder = new SimilarityFinder(stub);
+	}
+	
+	@Test
+	public void shouldReturnOneWhenSequencesAreEqual() {
+		int[] seq1 = {1,2,3,4,5,6,7};
+		int[] seq2 = {1,2,3,4,5,6,7};
+		
+		for(int i = 0 ; i < seq1.length ; i++) {
+			stub.getIsFound().add(true);
+		}
+		
+		double expectedResult = finder.calculateJackardSimilarity(seq1, seq2);
+		
+		assertThat(expectedResult, is(1.0));
+	}
 }
