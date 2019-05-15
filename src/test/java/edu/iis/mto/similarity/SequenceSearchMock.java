d0e86767ee22f6e50f0b013d7f1c6d74ce262668
Jakub Mroczek <jakubmroczek2@gmail.com>
2019-05-15 19:43:25 +0200

    switch to List instead of Supplier in SequenceSearchMock

--- src/test/java/edu/iis/mto/similarity/SequenceSearchMock.java
+++ src/test/java/edu/iis/mto/similarity/SequenceSearchMock.java
@@ -3,18 +3,21 @@ package edu.iis.mto.similarity;
 import edu.iis.mto.search.SearchResult;
 import edu.iis.mto.search.SequenceSearcher;
 
-import java.util.function.Supplier;
+import java.util.Iterator;
+import java.util.List;
 
 public class SequenceSearchMock implements SequenceSearcher {
 
-    private Supplier<Boolean> searchFoundSupplier = () -> {throw new IllegalArgumentException("SequenceSearchMock SearchFoundSupplier had not been provided");};
+    private List<Boolean> results;
+    private Iterator<Boolean> iterator;
 
     @Override
     public SearchResult search(int key, int[] seq) {
-        return SearchResult.builder().withFound(searchFoundSupplier.get()).build();
+        return SearchResult.builder().withFound(iterator.next()).build();
     }
 
-    public void setSearchFoundSupplier(Supplier<Boolean> searchFoundSupplier) {
-        this.searchFoundSupplier = searchFoundSupplier;
+    public void setResults(List<Boolean> results) {
+        this.results = results;
+        this.iterator = results.iterator();
     }
 }
