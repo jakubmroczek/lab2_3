52bcaf8b30b3e5069bf05e12164988b3ddb9a442
Jakub Mroczek <jakubmroczek2@gmail.com>
2019-05-15 19:45:20 +0200

    added computational test

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -1,8 +1,11 @@
 package edu.iis.mto.similarity;
 
-import edu.iis.mto.search.SequenceSearcher;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Supplier;
 
 import static org.junit.Assert.*;
 
@@ -11,7 +14,7 @@ public class SimilarityFinderTest {
     private SimilarityFinder sut;
     private SequenceSearchMock sequenceSearchMock;
 
-    private static final double EPSILON = 0.0000000001;
+    private static final double EPSILON = 0.1;
 
     @Before
     public void setUp() {
@@ -20,12 +23,32 @@ public class SimilarityFinderTest {
     }
 
     @Test
-    public void shouldJackardIndexBeOneForEqualArrays() {
+    public void shouldJaccardIndexBeOneForEqualArrays() {
         int[] first = {1, 2, 3, 4};
         int[] second = {1, 2, 3, 4};
 
-        sequenceSearchMock.setSearchFoundSupplier(() -> true);
+        sequenceSearchMock.setResults(Arrays.asList(true, true, true, true));
 
         assertEquals(sut.calculateJackardSimilarity(first, second), 1.0, EPSILON);
+    }
+
+    @Test
+    public void shouldJaccardIndexBeOneForBothEmptyArrays() {
+        int[] first = {};
+        int[] second = {};
+
+        sequenceSearchMock.setResults(Arrays.asList(true));
+
+        assertEquals(sut.calculateJackardSimilarity(first, second), 1.0, EPSILON);
+    }
+
+    @Test
+    public void shouldReturnProperJaccardIndex() {
+        int[] first = {1, 2, 3, 4, 5, 8, 9};
+        int[] second = {5, 8, 9};
+
+        sequenceSearchMock.setResults(Arrays.asList(false, false, false, false, true, true, true));
+
+        assertEquals(sut.calculateJackardSimilarity(first, second), 0.43, EPSILON);
     }
 }
