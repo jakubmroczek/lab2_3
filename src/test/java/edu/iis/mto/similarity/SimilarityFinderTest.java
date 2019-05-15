841934967cb0279b9ea4a6b50eff235a7fecc7bd
Jakub Mroczek <jakubmroczek2@gmail.com>
2019-05-15 19:16:30 +0200

    implemented test case for equal arrays

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -1,8 +1,31 @@
 package edu.iis.mto.similarity;
 
+import edu.iis.mto.search.SequenceSearcher;
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class SimilarityFinderTest {
 
+    private SimilarityFinder sut;
+    private SequenceSearchMock sequenceSearchMock;
 
+    private static final double EPSILON = 0.0000000001;
+
+    @Before
+    public void setUp() {
+        sequenceSearchMock = new SequenceSearchMock();
+        sut = new SimilarityFinder(sequenceSearchMock);
+    }
+
+    @Test
+    public void shouldJackardIndexBeOneForEqualArrays() {
+        int[] first = {1, 2, 3, 4};
+        int[] second = {1, 2, 3, 4};
+
+        sequenceSearchMock.setSearchFoundSupplier(() -> true);
+
+        assertEquals(sut.calculateJackardSimilarity(first, second), 1.0, EPSILON);
+    }
 }
